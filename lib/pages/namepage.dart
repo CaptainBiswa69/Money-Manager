8f2ba5b546b8abf55d262a579d111377bcf651f7
CaptainBiswa69 <pradipchadra97@gmail.com>
2022-03-27 21:42:10 +0530

    Scrollable Done

--- lib/pages/namepage.dart
+++ lib/pages/namepage.dart
@@ -24,58 +24,60 @@ class _NamePAgeState extends State<NamePAge> {
       appBar: AppBar(
         toolbarHeight: 0.0,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/log.png",
-                width: 300,
-                height: 300,
-              ),
-              Text(
-                "Welcome",
-                style: TextStyle(
-                  color: Static.PrimaryColor,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/log.png",
+                  width: 300,
+                  height: 300,
                 ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                margin: EdgeInsets.all(20),
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'What should we call you?',
+                Text(
+                  "Welcome",
+                  style: TextStyle(
+                    color: Static.PrimaryColor,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      _addName(value);
-                      nameCheck = value;
-                    });
-                  },
                 ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                    margin: EdgeInsets.symmetric(horizontal: 20),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          if (nameCheck.isNotEmpty) {
-                            Navigator.of(context).pushReplacement(
-                                MaterialPageRoute(
-                                    builder: (context) => FingerprintAuth()));
-                          }
-                        },
-                        child: Icon(Icons.arrow_right_alt)),
-                  )),
-                ],
-              )
-            ],
+                SizedBox(height: 30),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'What should we call you?',
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _addName(value);
+                        nameCheck = value;
+                      });
+                    },
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 20),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if (nameCheck.isNotEmpty) {
+                              Navigator.of(context).pushReplacement(
+                                  MaterialPageRoute(
+                                      builder: (context) => FingerprintAuth()));
+                            }
+                          },
+                          child: Icon(Icons.arrow_right_alt)),
+                    )),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
